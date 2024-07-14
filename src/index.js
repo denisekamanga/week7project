e4573fbeb499ac24494e08ea3453ace462610239
denisekamanga <110811633+denisekamanga@users.noreply.github.com>
2024-07-14 15:50:51 +0300

    updated search functionality with api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
+function updateweatherinfo(response){
+
+    console.log(response.data);
+
+    let temp = response.data.temperature.current;
+    let oldtemp = document.querySelector("#weather-app-temp");
+    oldtemp.innerHTML = Math.round(temp);
+
+}
+
+function searchCity(city){
+    let apikey = "f21c03107be2b6tbd1077of9aa76543f";
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+
+    axios.get(url).then(updateweatherinfo);
+
+  
+}
+
+
 function handleSearchSubmit(event){
 
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let city = document.querySelector("#weather-app-city");
-    city.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value)
 };
 
 let serchFormElement = document.querySelector("#searchForm");
